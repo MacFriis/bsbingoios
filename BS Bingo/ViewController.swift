678a26aa1e6067e0e0047c24113173f615f38776
Per Friis <per.friis@friisconsult.com>
2018-10-19 13:31:28 +0200

    Initial Commit

---
+++ BS Bingo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BS Bingo
+//
+//  Created by Per Friis on 19/10/2018.
+//  Copyright © 2018 Per Friis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
